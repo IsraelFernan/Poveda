4ae91f71ffce3bd9247f159da374ad63b5c7826d
IsraelFernan <alexlopescado@gmail.com>
2022-07-25 18:35:19 -0500

    P_ValidacionCedula

--- src/app/Components/ingreso/ingreso.component.ts
+++ src/app/Components/ingreso/ingreso.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class IngresoComponent implements OnInit {
 
   validador: boolean=false;
-
+  cedula: String = '';
   constructor() { }
 
   validadorDeCedula(cedula: String) {
