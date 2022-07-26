659c852731a4599045826c070923c68d8e33a5f0
IsraelFernan <alexlopescado@gmail.com>
2022-07-25 19:16:05 -0500

    P_CSS

--- src/app/Components/ingreso/ingreso.component.ts
+++ src/app/Components/ingreso/ingreso.component.ts
@@ -7,52 +7,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class IngresoComponent implements OnInit {
 
-  validador: boolean=false;
-  cedula: String = '';
-  mayusculas: any = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-  minusculas: any = 'abcdefghijklmnopqrstuvwxyz';
-  caracteres: any  = '@#$%&';
+  
   constructor() { }
 
-  validadorDeCedula(cedula: String) {
-    let cedulaCorrecta = false;
-
-    if (cedula.length == 10) {
-      let tercerDigito = parseInt(cedula.substring(2, 3));
-      if (tercerDigito < 6) {
-
-        // El ultimo digito se lo considera dígito verificador
-        let coefValCedula = [2, 1, 2, 1, 2, 1, 2, 1, 2];
-        let verificador = parseInt(cedula.substring(9, 10));
-        let suma: number = 0;
-        let digito: number = 0;
-        for (let i = 0; i < (cedula.length - 1); i++) {
-          digito = parseInt(cedula.substring(i, i + 1)) * coefValCedula[i];
-          suma += ((parseInt((digito % 10) + '') + (parseInt((digito / 10) + ''))));
-          //      console.log(suma+" suma"+coefValCedula[i]); 
-        }
-
-        suma = Math.round(suma);
-
-        //  console.log(verificador);
-        //  console.log(suma);
-        //  console.log(digito);
-
-        if ((Math.round(suma % 10) == 0) && (Math.round(suma % 10) == verificador)) {
-          cedulaCorrecta = true;
-        } else if ((10 - (Math.round(suma % 10))) == verificador) {
-          cedulaCorrecta = true;
-        } else {
-          cedulaCorrecta = false;
-        }
-      } else {
-        cedulaCorrecta = false;
-      }
-    } else {
-      cedulaCorrecta = false;
-    }
-    this.validador = cedulaCorrecta;
-  }
+  
   ngOnInit(): void {
   }
 
