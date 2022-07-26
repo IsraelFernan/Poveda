5e607f341e82a76993e9d7d066cc4ce9ce468432
IsraelFernan <alexlopescado@gmail.com>
2022-07-25 19:47:28 -0500

    P_Clave

--- src/app/Components/registro/registro.component.ts
+++ src/app/Components/registro/registro.component.ts
@@ -17,6 +17,40 @@ export class RegistroComponent implements OnInit {
   clave: string = '';
   constructor() { }
 
+  validacionClave(){
+    var i =0;
+    while(this.clave.length > i){
+      var j=0;
+      var k=0;
+      var l=0;
+      var m=0;
+      var n=0;
+      var c=0;
+      while(this.mayusculas.length > j){
+        if(this.clave[i] == this.mayusculas[j]){
+          m++;
+        }
+        j++;
+      }
+      while(this.minusculas.length > k){
+        if(this.clave[i] == this.minusculas[k]){
+          n++;
+        }
+        k++;
+      }
+      while(this.caracteres.length > l){
+        if(this.clave[i] == this.caracteres[l]){
+          c++;
+        }
+        l++;
+      }
+      if(m <= 2 || n <= 2 || c <= 2){
+        alert('Por favor reingrese la clave.')
+      }
+      i++;
+    }
+  }
+
   validadorDeCedula(cedula: String) {
     let cedulaCorrecta = false;
 
